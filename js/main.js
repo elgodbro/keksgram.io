93d48afecb5f5917ef4140f98ca237d3506f2b57
ElgodBro <elgodbro@gmail.com>
2023-09-14 17:12:29 +0300

    Changes to be committed: 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 'use strict';
 
-let uploadedPhotos = [];
+const uploadedPhotos = [];
 
-let descriptions = [
+const IMAGES_COUNT = 25;
+const USERS_COUNT = 6;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MAX_COMMENTS = 4;
+
+const descriptions = [
     'Тестим новую камеру!',
     'Затусили с друзьями на море',
     'Как же круто тут кормят',
@@ -11,7 +17,7 @@ let descriptions = [
     'Вот это тачка!',
 ];
 
-let messagesInComments = [
+const messagesInComments = [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
     'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -20,82 +26,80 @@ let messagesInComments = [
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-let names = ['Шура', 'Никифор', 'Тимур', 'Вова', 'Егор', 'Иван', 'Ферапонт', 'Тёма', 'Рома', 'Ирений', 'Паша', 'Влад', 'Саша', 'Боря', 'Володя', 'Лев', 'Слава', 'Макс', 'Абрам', 'Филипп', 'Артур'];
+const names = ['Шура', 'Никифор', 'Тимур',
+               'Вова', 'Егор', 'Иван',
+               'Ферапонт', 'Тёма', 'Рома',
+               'Ирений', 'Паша', 'Влад',
+               'Саша', 'Боря', 'Володя',
+               'Лев', 'Слава', 'Макс',
+               'Абрам', 'Филипп', 'Артур'];
 
-let pictureTemplate = document.querySelector('#picture'), pictureFragment = new DocumentFragment();
+const pictureTemplate = document.querySelector('#picture');
+const pictureFragment = new DocumentFragment();
+
 
 function generateNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function picturesGenerateDOM(data) {
-    // pictureClone.querySelector('.picture__img').src = data[generateNumber(0, data.length - 1)].url;
-    // pictureClone.querySelector('.picture__img').alt = data[generateNumber(0, data.length - 1)].description;
-    // pictureClone.querySelector('.picture__comments').textContent = data[generateNumber(0, data.length - 1)].comments.length;
-    // pictureClone.querySelector('.picture__likes').textContent = data[generateNumber(0, data.length - 1)].likes;
-
-    //Не понимаю зачем разбивать этот фрагмент на две функции,
-    //когда можно воспользоваться лишь одной, но видимо так для чего-то нужно
-
-    let picture = picturesSetData(data[generateNumber(0, data.length - 1)].url, data[generateNumber(0, data.length - 1)].description, data[generateNumber(0, data.length - 1)].comments.length, data[generateNumber(0, data.length - 1)].likes);
-
-    pictureFragment.appendChild(picture);
+    for (let k = 0; k < data.length; k++) {
+        pictureFragment.appendChild(picturesSetData(data[k]));
+    }
 }
 
-function picturesSetData(src, alt, comm, likes) {
+function picturesSetData(data) {
     let pictureClone = pictureTemplate.content.cloneNode(true);
-    pictureClone.querySelector('.picture__img').src = src;
-    pictureClone.querySelector('.picture__img').alt = alt;
-    pictureClone.querySelector('.picture__comments').textContent = comm;
-    pictureClone.querySelector('.picture__likes').textContent = likes;
+    pictureClone.querySelector('.picture__img').src = data.url;
+    pictureClone.querySelector('.picture__img').alt = data.description;
+    pictureClone.querySelector('.picture__comments').textContent = data.comments.length;
+    pictureClone.querySelector('.picture__likes').textContent = data.likes;
 
     return pictureClone;
 }
 
 function loadBigPicture(photo) {
-    let bigPicture = document.querySelector('.big-picture');
-    bigPicture.classList.remove('hidden');
+    const bigPicture = document.querySelector('.big-picture');
 
     bigPicture.querySelector('.big-picture__img>img').src = photo.url;
     bigPicture.querySelector('.likes-count').textContent = photo.likes;
     bigPicture.querySelector('.comments-count').textContent = photo.comments.length;
     bigPicture.querySelector('.social__caption').textContent = photo.description;
 
-    let comments = "";
-    photo.comments.forEach(comment => {
-        comments += `<li class="social__comment">
-        	  <img class="social__picture" src="${comment.avatar}"
-        	    alt="${comment.name}"
-        	    width="35" height="35">
-        	  <p class="social__text">${comment.message}</p>
+    const comments = photo.comments.map(comment => {
+        return `<li class="social__comment">
+          <img class="social__picture" src="${comment.avatar}"
+            alt="${comment.name}"
+            width="35" height="35">
+          <p class="social__text">${comment.message}</p>
         </li>`;
-    });
+      }).join('');
 
     bigPicture.querySelector('.social__comments').innerHTML = comments;
 
     document.querySelector('.social__comment-count').classList.add('visually-hidden');
     document.querySelector('.comments-loader').classList.add('visually-hidden');
+    bigPicture.classList.remove('hidden');
 }
 
-for (let i = 1; i <= 25; i++) {
+for (let i = 1; i <= IMAGES_COUNT; i++) {
     let comments = [];
-    for (let j = 0; j <= generateNumber(0, 3); j++) {
+    for (let j = 0; j <= generateNumber(0, MAX_COMMENTS); j++) {
         comments.push({
-            avatar: `img/avatar-${generateNumber(1, 6)}.svg`,
-            message: messagesInComments[generateNumber(0, messagesInComments.length - 1)],
-            name: names[generateNumber(0, names.length - 1)]
+            avatar: `img/avatar-${generateNumber(1, USERS_COUNT)}.svg`,
+            message: messagesInComments[generateNumber(0, messagesInComments.length)],
+            name: names[generateNumber(0, names.length)],
         });
     }
     uploadedPhotos.push({
-        url: `photos/${generateNumber(1, 25)}.jpg`,
-        likes: generateNumber(15, 200),
-        description: descriptions[generateNumber(0, descriptions.length - 1)],
+        url: `photos/${i}.jpg`,
+        likes: generateNumber(MIN_LIKES, MAX_LIKES),
+        description: descriptions[generateNumber(0, descriptions.length)],
         comments: comments
     });
-
-    picturesGenerateDOM(uploadedPhotos);
 }
 
+picturesGenerateDOM(uploadedPhotos);
 document.querySelector('.pictures').appendChild(pictureFragment);
 
 loadBigPicture(uploadedPhotos[0]);
